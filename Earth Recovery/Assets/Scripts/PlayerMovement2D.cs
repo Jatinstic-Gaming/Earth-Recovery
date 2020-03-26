4ce19b4c57ae04df03e6c491b94be1d92e023bc3
JatinsticGaming <62481751+JatinsticGaming@users.jatinstic.github.com>
2020-03-26 23:13:25 +0530

    Added SpriteSheet, TileSet, Scripts and Some Other Things...

---
+++ Earth Recovery/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : CharacterController2D
+{
+    private float JumpForce = 10f;
+    private float MoveSpeed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 Move = Vector2.zero;
+
+        Move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("Jump") && IsGrounded)
+        {
+            Velocity.y = JumpForce;
+        }
+
+        TargetVelocity = Move * MoveSpeed;
+    }
+}
