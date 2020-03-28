e452237abad103a08e6d9c60188ece143a20b8b5
JatinsticGaming <62481751+JatinsticGaming@users.jatinstic.github.com>
2020-03-28 22:40:07 +0530

    Added More Sprites and Animations

--- Earth Recovery/Assets/Scripts/PlayerMovement2D.cs
+++ Earth Recovery/Assets/Scripts/PlayerMovement2D.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement2D : CharacterController2D
 {
-    private float JumpForce = 20f;
+    private float JumpForce = 16f;
     private float MoveSpeed = 8f;
 
     // Start is called before the first frame update
